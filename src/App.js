9c6a44629856f6140fb10ab44d83b54e87060078
Anuroop Joy <anuroop.joy@hrblock.com>
2021-06-05 00:17:38 +0530

    chage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import './App.css';
-// import Calc from './Calc/Calc';
+// import { Calc } from './Calc/Calc';
 import CalcErrorBoundary from './CalcErrorBoundary';
-
 const Calc = React.lazy(() => import('./Calc'));
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
 				<CalcErrorBoundary>
-					<Suspense fallback={<div>Loading...</div>}>
+					<React.Suspense fallback={<div>Loading...</div>}>
 						<Calc></Calc>
-					</Suspense>
+					</React.Suspense>
 				</CalcErrorBoundary>
 			</header>
 		</div>
